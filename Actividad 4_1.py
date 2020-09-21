9f4736a29dff221452ac3d07ada9385a23bfec8c
Gustavo Gómez <luis.gomez6835@alumnos.udg.mx>
2020-09-20 21:04:21 -0500

    Funciones áreas de las figuras: cuadrado, triángulo y círculo

---
+++ Actividad 4_1.py
@@ -0,0 +1,34 @@
+from math import pi
+def cuadrado (a:float,b:float)->float:
+    area=a*b
+    return area
+def triangulo (a:float,b:float)->float:
+    area=(a*b)/2
+    return area
+def circulo (a:float)->float:
+    area = (a**a)*pi
+    return area  
+
+print("Elija una opción para calcular el área")
+while True:
+    print('1) Cuadrado')
+    print('2) Triangulo')
+    print('3) Círculo')
+    print('0) Salir')
+    op = input()
+
+    if op =="1":
+     a = float(input("Base: "))
+     b = float(input("Altura: "))
+     print("Área: ",cuadrado(a,b))
+    elif op =="2":
+     a = float(input("Base: "))
+     b = float(input("Altura: "))
+     print("Área: ",triangulo(a,b))
+
+    elif op =="3":
+     a = float(input("Radio: "))
+     print("Área: ",circulo(a))
+    
+    elif op == "0":
+        break
