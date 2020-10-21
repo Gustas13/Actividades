32b16faa83bd106d15eeb3e2a0a503ed7f2faa77
Gustavo Gómez <luis.gomez6835@alumnos.udg.mx>
2020-10-21 13:52:41 -0500

    Modificación de Algoritmos

--- Actividad9/algoritmos.py
+++ Actividad9/algoritmos.py
@@ -1,9 +1,8 @@
 import math 
-
 def distancia_euclidiana(x_1, y_1, x_2, y_2):
     x = x_2 - x_1
     x1=pow(x,2)
     y = y_2 - y_1
     y1=pow(y,2)
     resultado = x1+y1
-    return(math.sqrt(resultado))
+    return (math.sqrt(resultado))
