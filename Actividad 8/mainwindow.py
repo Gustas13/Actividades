2cc9daeea4c8138d41de513d07ffc3387d5975c3
Gustavo Gómez <luis.gomez6835@alumnos.udg.mx>
2020-10-28 12:22:33 -0600

    Actualización de mainwondow.py Actividad 11

--- Actividad 8/mainwindow.py
+++ Actividad 8/mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 from administradorP import AdministradorP
@@ -15,7 +15,53 @@ class MainWindow(QMainWindow):
         self.ui.Agregar_Inicio_pushButton.clicked.connect(self.click_agregar)
         self.ui.Agregar_Final_pushButton.clicked.connect(self.click_agregar_final)
         self.ui.Mostar_pushButton.clicked.connect(self.click_mostrar)
+
+        self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
+        self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
+    
     @Slot()
+    def action_abrir_archivo(self):
+        ubicacion = QFileDialog.getOpenFileName(
+            self,
+            'Abrir Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        if self.administradorP.abrir(ubicacion):
+            QMessageBox.information(
+                self,
+                "Éxito",
+                "Se abrio el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "Errro al abrir el archivo " + ubicacion
+            )
+        #print('abiri_archivo')
+    
+    def action_guardar_archivo(self):
+        #print('guardar_archivo')
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            'Guardar Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        print(ubicacion)
+        if self.administradorP.guardar(ubicacion):
+            QMessageBox.information(
+                self,
+                "Éxito",
+                "Se pudo crear el archivo" + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "No se pudo crear el archivo"+ubicacion
+            )
     def click_mostrar(self):
         #self.administradorP.mostrar()
         self.ui.salida.clear()
