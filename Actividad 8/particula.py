f0efbc72cc4f67c67a5f9083080834387fe03df9
Gustavo Gómez <luis.gomez6835@alumnos.udg.mx>
2020-10-28 12:46:37 -0600

    Actualización de Paricula Actividad 11

--- Actividad 8/particula.py
+++ Actividad 8/particula.py
@@ -25,3 +25,16 @@ class Particula:
             'Blue: ' + str (self.__blue) + '\n' +
             'Distancia: ' + str(self.__distancia) + '\n'
         )
+    
+    def to_dict(self):
+        return {
+            "id": self.__id,
+	        "origen_x": self.__origen_x,
+            "origen_y": self.__origen_y,
+	        "destino_x": self.__destino_x,
+	        "destino_y": self.__destino_x,
+	        "velocidad": self.__velocidad,
+	        "red": self.__red,
+	        "green": self.__blue,
+	        "blue": self.__green
+        }
